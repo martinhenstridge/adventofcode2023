b8007df828c70fddc0231fa509e3c76d9e54764a
Martin Henstridge <mchenstridge@gmail.com>
2023-12-16 23:59:49 +0000

    part 2

--- aoc/day14.py
+++ aoc/day14.py
@@ -1,38 +1,70 @@
-def load(sequence, length):
-    return sum(length - i for i, item in enumerate(sequence) if item == "O")
+import itertools
+
+
+def load(cols):
+    return sum(len(col) - i for col in cols for i, item in enumerate(col) if item == "O")
 
 
 def roll(col):
-    round_rocks = 0
     empty_space = 0
 
     for item in col:
         if item == ".":
             empty_space += 1
         elif item == "O":
-            round_rocks += 1
+            yield item
         elif item == "#":
-            for _ in range(round_rocks):
-                yield "O"
             for _ in range(empty_space):
                 yield "."
-            round_rocks = 0
             empty_space = 0
             yield item
-    for _ in range(round_rocks):
-        yield "O"
     for _ in range(empty_space):
         yield "."
 
 
+def cycle(cols):
+    for _ in range(4):
+        cols = rotated(roll(col) for col in cols)
+    return cols
+
+
+def rotated(cols):
+    return list(reversed(["".join(row) for row in zip(*cols)]))
+
+
+def dump(cols):
+    print("--")
+    for row in zip(*cols):
+        print("".join(row))
+
+
+def find_nth_cycle(cols, n):
+    history = {}
+    for i in itertools.count():
+        signature = "".join(cols)
+        print(i, load(cols))
+        if signature in history:
+            break
+        history[signature] = i
+        cols = cycle(cols)
+
+    offset = history[signature]
+    period = i - offset
+    index = offset + ((n - offset) % period)
+    print("offset:", offset)
+    print("period:", period)
+    print("index:", index)
+    return index
+
+
+
 def run(data):
     #data = DATA
-    rows = data.splitlines()
-    total = 0
-    for col in zip(*rows):
-        rolled = roll(col)
-        total += load(rolled, len(col))
-    return total, 0
+    cols = ["".join(row) for row in zip(*data.splitlines())]
+
+    n = find_nth_cycle(cols, 1000000000)
+    print(n)
+    return 0, 0
 
 
 DATA = """\
