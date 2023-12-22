c3a73c33770deb475c6398ff0137d3a3e449ee4f
Martin Henstridge <mchenstridge@gmail.com>
2023-12-22 12:12:15 +0000

    Day 21 part 2

--- aoc/day21.py
+++ aoc/day21.py
@@ -20,7 +20,38 @@ def neighbours(position):
 
 
 def step(garden, current):
-    return {n for p in current for n in neighbours(p) if garden[n] != "#"}
+    return {
+        (r, c)
+        for p in current
+        for r, c in neighbours(p)
+        if garden[r % 131, c % 131] != "#"
+    }
+
+
+def extrapolate_plots(steps):
+    # Extrapolation based on the following observations / assumptions:
+    #
+    # - The garden is 131 x 131
+    # - The starting point is the centre (65, 65)
+    # - The path to the edge both horizontally and vertically is clear
+    # - It takes 65 steps to reach the edge of the first tile
+    # - It takes a further 131 steps to reach the edge of each subsequent tile
+    # - A pattern is expected starting at 65 steps with a period of 131 steps
+    # - The area (and hence reachable plots) is quadratic in number of steps
+    #
+    # Running the naive solution for:
+    # - 65 steps  (= 65 + 0*131)
+    # - 196 steps (= 65 + 1*131)
+    # - 327 steps (= 65 + 2*131)
+    # provides three points from which the quadratic solution may be derived.
+    #
+    # n  s    p
+    # 0  65   3885
+    # 1  196  34700
+    # 2  327  96215
+
+    n = (steps - 65) // 131
+    return 3885 + 15465 * n + 15350 * n * n
 
 
 def run(data):
@@ -30,4 +61,4 @@ def run(data):
     for _ in range(64):
         plots = step(garden, plots)
 
-    return len(plots), 0
+    return len(plots), extrapolate_plots(26501365)
